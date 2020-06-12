c248d624a55b4114fc6fcabbc0d9b042d14e8b4c
Benedikt Ritter <beneritter@gmail.com>
2020-06-12 09:37:15 +0200

    Render a list of items

--- src/Main.elm
+++ src/Main.elm
@@ -7,39 +7,68 @@ module Main exposing (..)
 --
 
 import Browser
-import Html exposing (Html, button, div, text)
+import Html exposing (Html, button, div, li, text, ul)
 import Html.Events exposing (onClick)
 
+
+
 -- MAIN
+
+
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+    Browser.sandbox { init = init, update = update, view = view }
+
+
 
 -- MODEL
-type alias Model = Int
+
+
+type alias Model =
+    List Int
+
 
 init : Model
 init =
-  0
+    [ 0 ]
+
+
 
 -- UPDATE
+
+
 type Msg
-  = Increment
-  | Decrement
+    = Increment
+    | Decrement
+
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Increment ->
-      model + 1
+    case msg of
+        Increment ->
+            model ++ [ 1 ]
 
-    Decrement ->
-      model - 1
+        Decrement ->
+            []
+
+
 
 -- VIEW
+
+
+renderList : Model -> Html Msg
+renderList model =
+    ul [] (List.map renderListItem model)
+
+
+renderListItem : Int -> Html Msg
+renderListItem x =
+    li [] [ text (String.fromInt x) ]
+
+
 view : Model -> Html Msg
 view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+        , renderList model
+        , button [ onClick Increment ] [ text "+" ]
+        ]
