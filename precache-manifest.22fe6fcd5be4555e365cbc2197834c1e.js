fa8d8a28dfc3d331a861bcd5522cafd778b89688
britter <beneritter@gmail.com>
2020-06-16 09:05:36 +0000

    Deploying to gh-pages from  @ 89714d3d712e3e2d507adae2ba0a33de934252ce 🚀

---
+++ precache-manifest.22fe6fcd5be4555e365cbc2197834c1e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "2b9aef0e0ab5532de88ccf8f5f12d087",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "dddb730ebe4ceeade8ab",
+    "url": "/static/css/main.7f457dfb.chunk.css"
+  },
+  {
+    "revision": "dddb730ebe4ceeade8ab",
+    "url": "/static/js/main.dddb730e.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "b9063112a1dd4b93007d",
+    "url": "/static/js/vendors~main.b9063112.chunk.js"
+  }
+]);
