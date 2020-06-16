19cae67975e4a0f4bd564610c6abf83d19d5cbdd
Benedikt Ritter <beneritter@gmail.com>
2020-06-16 19:52:30 +0200

    Add some tests for score calculations

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,6 +1,7 @@
 module Tests exposing (..)
 
 import Expect
+import Fuzz exposing (Fuzzer, intRange)
 import Main
 import Test exposing (..)
 
@@ -15,6 +16,66 @@ all =
         [ describe "init"
             [ test "Init creates empty setup model" <|
                 \_ ->
-                    Expect.equal Main.init (Main.Setup { currentNameInput = Nothing, players = [] })
+                    let
+                        expected =
+                            Main.Setup { currentNameInput = Nothing, players = [] }
+                    in
+                    Expect.equal Main.init expected
+            ]
+        , describe "calculate score"
+            [ fuzz scorableValues "positive scores are added" <|
+                \score ->
+                    Expect.equal (Main.calculateScores [] score) [ score ]
+            , describe "on third miss"
+                [ test "subtracts 15 if score is higher than 15" <|
+                    \_ ->
+                        let
+                            scores =
+                                [ 0, 0, 10, 10 ]
+
+                            expected =
+                                -15 :: scores
+                        in
+                        Expect.equal (Main.calculateScores scores 0) expected
+                , test "reduces to 0 if score is below 15" <|
+                    \_ ->
+                        let
+                            scores =
+                                [ 0, 0, 1 ]
+
+                            expected =
+                                -1 :: scores
+                        in
+                        Expect.equal (Main.calculateScores scores 0) expected
+                ]
+            , describe "on approaching 50 points"
+                [ fuzz canWinTotal "when scoring more than 50 points are reduced to 25" <|
+                    \previousTotal ->
+                        let
+                            score =
+                                51 - previousTotal
+
+                            penalty =
+                                -(previousTotal - 25)
+                        in
+                        Expect.equal (Main.calculateScores [ previousTotal ] score) [ penalty, previousTotal ]
+                , fuzz canWinTotal "when scoring exactly 50 the score is added" <|
+                    \previousTotal ->
+                        let
+                            score =
+                                50 - previousTotal
+                        in
+                        Expect.equal (Main.calculateScores [ previousTotal ] score) [ score, previousTotal ]
+                ]
             ]
         ]
+
+
+scorableValues : Fuzzer Int
+scorableValues =
+    intRange 0 12
+
+
+canWinTotal : Fuzzer Int
+canWinTotal =
+    intRange 39 49
